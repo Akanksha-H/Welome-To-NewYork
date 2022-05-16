2905753b7126ed4101c02d501c62fa9642f2c9c2
Aakanksha <aakanksha0499@gmail.com>
2022-05-16 16:04:08 +0530

    City Location

--- src/Testing.java
+++ src/Testing.java
@@ -1,5 +1,6 @@
 public class Testing {
     public static void main(String[] args) {
         System.out.println("WELCOME_TO_NEW_YORK");
+        System.out.println("WELCOME_TO_NEW_YORK-IN_wall-mart");
     }
 }
